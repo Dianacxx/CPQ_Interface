2c0b776ba424a347c9b43ce2c5d8bfda0aa24602
CaroMercado <carolina@bricklane.ai>
2021-12-29 14:48:06 -0500

    Working in PS UI

--- force-app/main/default/lwc/bl_dataTable/bl_dataTable.js
+++ force-app/main/default/lwc/bl_dataTable/bl_dataTable.js
@@ -314,6 +314,7 @@ export default class Bl_dataTable extends LightningElement {
             });
             this.dispatchEvent(evt);
         }
+        this.firstHandler();
         //MISSING WHEN UPDATE FIELDS IN NOTES
         /*
         *
